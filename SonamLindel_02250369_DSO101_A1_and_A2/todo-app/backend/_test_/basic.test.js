0d9e3dd4f44f69f57e7464d3cf8627971fb45496
lindel19 <sonamlindel@agmail.com>
2026-05-06 15:16:55 +0600

    f

---
+++ SonamLindel_02250369_DSO101_A1_and_A2/todo-app/backend/_test_/basic.test.js
@@ -0,0 +1,3 @@
+test("basic test should pass", () => {
+  expect(1 + 1).toBe(2);
+});
---
+++ SonamLindel_02250369_DSO101_A1_and_A2/todo-app/backend/server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+// GET
+app.get("/tasks", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM tasks ORDER BY id ASC");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("DB ERROR:", err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// POST
+app.post("/tasks", async (req, res) => {
+  try {
+    const { title } = req.body;
+
+    const result = await pool.query(
+      "INSERT INTO tasks (title) VALUES ($1) RETURNING *",
+      [title]
+    );
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error("DB ERROR:", err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// PUT
+app.put("/tasks/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, completed } = req.body;
+
+    const result = await pool.query(
+      "UPDATE tasks SET title=$1, completed=$2 WHERE id=$3 RETURNING *",
+      [title, completed, id]
+    );
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+     console.error("DB ERROR:", err.message);
+     res.status(500).json({ error: err.message });
+  }
+});
+
+// DELETE
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await pool.query("DELETE FROM tasks WHERE id=$1", [id]);
+
+    res.json({ message: "Deleted" });
+  } catch (err) {
+    console.error("DB ERROR:", err.message);
+    res.status(500).json({ error: "DB error" });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
